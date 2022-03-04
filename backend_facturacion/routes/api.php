cccb85bdd369fea59ffeaf6b1ceaf96572cd441c
JustGust <gusalomia@gmail.com>
2022-03-04 10:44:20 -0500

    api backend casi listo

--- backend_facturacion/routes/api.php
+++ backend_facturacion/routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+   // Route::post('refresh', 'AuthController@refresh');
+    //Route::post('me', 'AuthController@me');
+
+});
